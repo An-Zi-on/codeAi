d835767366a8c91d2b5d37d7c836768971687726
HeZiAn <15017470458@163.com>
2025-11-17 10:15:12 +0800

    -项目初始化

---
+++ codeAi/src/main/java/com/aizihe/codeaAI/CodeAiApplication.java
@@ -0,0 +1,13 @@
+package com.aizihe.codeaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAiApplication.class, args);
+    }
+
+}
