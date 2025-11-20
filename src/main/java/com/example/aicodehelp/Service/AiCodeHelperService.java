1db6e154769056781a3555232a747b6046443aba
HeZiAn <15017470458@163.com>
2025-11-20 10:59:58 +0800

    add:1.注释添加

--- src/main/java/com/example/aicodehelp/Service/AiCodeHelperService.java
+++ src/main/java/com/example/aicodehelp/Service/AiCodeHelperService.java
@@ -3,7 +3,11 @@ package com.example.aicodehelp.Service;
 import dev.langchain4j.service.SystemMessage;
 
 public interface AiCodeHelperService {
-
+    /**
+     * 预设prompt
+     * @param userMessage
+     * @return
+     */
     @SystemMessage(fromResource = "system-prompt.txt")
     String chat(String userMessage);
 }
